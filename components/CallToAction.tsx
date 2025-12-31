2727d151d3ed1f56317aeeea6c442c1aaf91fae9
Anant Kr Sharma <anantkrsharma01@gmail.com>
2025-12-31 23:43:49 +0530

    chores: cal link

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -15,7 +15,7 @@ export default function CallToAction() {
     <div className="w-full flex items-center px-2 sm:px-4 ">
       <Cal 
     namespace="openlens-discussion"
-    calLink="dilpreet-grover/odena-discussion"
+    calLink="paranjai-g-slf80z/30min"
     style={{width:"100%",height:"100%",overflow:"hidden"}}
     config={{"layout":"month_view"}}
     />
